a93cc88179e2084fdf82307368e86cd951c4b297
WalterJhonSalibay <salibaywaltz@gmail.com>
2024-10-19 17:49:55 +0800

    first commit

---
+++ basic.py
@@ -0,0 +1,12 @@
+# variables
+one = 10
+two = 20
+
+# print Try Basic Commands on Your Own Machine
+print("Try basic Commands on Your Own Machine")
+
+# add 3 + 3
+print (3 + 3)
+
+# add one and two variables
+print(one + one)
---
+++ controlstructure.py
@@ -0,0 +1,10 @@
+# variables
+name = "Walter"
+age = "24"
+
+if name == "Nice":
+    print("Hello" + name)
+elif name == "Walter" and age == 24:
+    print("Wow Hello" + name)
+else:
+    print("No match")
